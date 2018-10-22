78c31dd6425489819f0b9cd084544ca4320520e1
simonherlin <herlin.simon@gmail.com>
2018-10-22 18:15:28 +0200

    [ADD] exception 404

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,3 +1,5 @@
+from django.shortcuts import get_object_or_404, render
+from django.http import Http404
 from django.shortcuts import render
 
 from .models import Question
@@ -8,8 +10,10 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    context = {'question_id': question_id}
-    return render(request, 'polls/detail.html', context)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+    # context = {'question_id': question_id}
+    # return render(request, 'polls/detail.html', context)
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
